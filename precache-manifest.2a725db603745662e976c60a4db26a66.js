bbe373d5aa973ee92ccbc6af94c6412739fa1912
Alex <alex@yang2k.com>
2019-09-01 12:27:52 -0500

    Updates

---
+++ precache-manifest.2a725db603745662e976c60a4db26a66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d09f45d425c277fceea4397fb799083",
+    "url": "/Helix-3/index.html"
+  },
+  {
+    "revision": "27a1168e08c542adba26",
+    "url": "/Helix-3/static/css/main.7a3d0273.chunk.css"
+  },
+  {
+    "revision": "c0a580ee09c99836da41",
+    "url": "/Helix-3/static/js/2.188531e5.chunk.js"
+  },
+  {
+    "revision": "27a1168e08c542adba26",
+    "url": "/Helix-3/static/js/main.6c395899.chunk.js"
+  },
+  {
+    "revision": "bbb45f5d4cd5a79e5315",
+    "url": "/Helix-3/static/js/runtime~main.65ef2eef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Helix-3/precache-manifest.a9c41cef645884fced0022f61f01844b.js"
+  "/Helix-3/precache-manifest.2a725db603745662e976c60a4db26a66.js"
 );
 
 self.addEventListener('message', (event) => {
