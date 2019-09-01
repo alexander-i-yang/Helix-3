b59bd13e181820f4178b2f3e4be0aee1def5d9ae
Alex <alex@yang2k.com>
2019-09-01 15:48:46 -0500

    Updates

--- precache-manifest.69a5c7b70ad8a792a4b5b0d421911f01.js
+++ precache-manifest.69a5c7b70ad8a792a4b5b0d421911f01.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8cf128c515a032d5a256d3347fa9a69c",
+    "revision": "27b2a1eca48d9ef3d940ef8d007ddc1a",
     "url": "/Helix-3/index.html"
   },
   {
-    "revision": "f2988c1db3b9b51bcc11",
-    "url": "/Helix-3/static/css/main.80e8a3aa.chunk.css"
+    "revision": "79abd7ef301aff80160f",
+    "url": "/Helix-3/static/css/main.3243e6e5.chunk.css"
   },
   {
     "revision": "c0a580ee09c99836da41",
     "url": "/Helix-3/static/js/2.188531e5.chunk.js"
   },
   {
-    "revision": "f2988c1db3b9b51bcc11",
+    "revision": "79abd7ef301aff80160f",
     "url": "/Helix-3/static/js/main.836352a9.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Helix-3/precache-manifest.0f827bae3847b0152784f7941681e692.js"
+  "/Helix-3/precache-manifest.69a5c7b70ad8a792a4b5b0d421911f01.js"
 );
 
 self.addEventListener('message', (event) => {
